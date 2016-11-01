5e00c2d05f64515c0160e7eb56f41f40f2f69bbe
EsProgram <radwimps.september@gmail.com>
2016-11-02 01:03:10 +0900

    Grab機能を使ったReflectionPaintサンプルの導入

---
+++ Assets/TexturePaint/Sample/Script/ReflectPainter.cs
@@ -0,0 +1,67 @@
+﻿using Es.TexturePaint;
+using System.Collections;
+using UnityEngine;
+
+public class ReflectPainter : MonoBehaviour
+{
+	[SerializeField]
+	private PaintBrush brush;
+
+	[SerializeField]
+	private GameObject camPref;
+
+	private RenderTexture rt;
+	private Camera cam;
+	private Vector2 uv;
+	private DynamicCanvas paintObject;
+
+	public void Awake()
+	{
+		rt = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+		brush.ColorBlending = PaintBrush.ColorBlendType.UseBrush;
+	}
+
+	public void OnGUI()
+	{
+		if(GUILayout.Button("Reset"))
+		{
+			if(paintObject != null)
+				paintObject.ResetPaint();
+			Destroy(cam);
+			cam = null;
+		}
+	}
+
+	private void Update()
+	{
+		if(cam == null && Input.GetMouseButtonDown(0))
+		{
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+			if(Physics.Raycast(ray, out hitInfo))
+			{
+				paintObject = hitInfo.transform.GetComponent<DynamicCanvas>();
+				if(paintObject != null)
+				{
+					uv = hitInfo.textureCoord;
+					var camObj = Instantiate(camPref, hitInfo.point, Quaternion.LookRotation(hitInfo.normal), hitInfo.transform) as GameObject;
+					cam = camObj.GetComponent<Camera>();
+					cam.targetTexture = rt;
+					camObj.SetActive(true);
+				}
+			}
+		}
+		else if(cam != null)
+		{
+			var buf = RenderTexture.GetTemporary(brush.BrushTexture.width, brush.BrushTexture.height);
+			Es.Effective.GrabArea.Clip(brush.BrushTexture, brush.Scale, rt, Vector3.one * 0.5f, Es.Effective.GrabArea.GrabTextureWrapMode.Clip, buf);
+			Es.Effective.ReverseUV.Horizontal(buf, buf);
+			var brushBuf = brush.BrushTexture;
+			brush.BrushTexture = buf;
+			if(paintObject != null)
+				paintObject.PaintUVDirect(brush, uv);
+			RenderTexture.ReleaseTemporary(buf);
+			brush.BrushTexture = brushBuf;
+		}
+	}
+}
---
+++ Assets/TexturePaint/Script/Effective/ReverseUV.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+namespace Es.Effective
+{
+	public class ReverseUV
+	{
+		#region PrivateField
+
+		private const string REVERSE_UV_SHADER = "Es/ReverseUV";
+		private const string REVERSE_X = "_ReverseX";
+		private const string REVERSE_Y = "_ReverseY";
+
+		private const float DEFAULT = 1f;
+		private const float REVERSE = 0f;
+
+		private static Material reverseUVMaterial = null;
+
+		#endregion PrivateField
+
+		#region PublicMethod
+
+		public static void Horizontal(Texture src, RenderTexture dst)
+		{
+			if(reverseUVMaterial == null)
+				InitReverseUVMaterial();
+			SetReverseUVProperty(REVERSE, DEFAULT);
+			Blit(src, dst);
+		}
+
+		public static void Vertical(Texture src, RenderTexture dst)
+		{
+			if(reverseUVMaterial == null)
+				InitReverseUVMaterial();
+			SetReverseUVProperty(DEFAULT, REVERSE);
+			Blit(src, dst);
+		}
+
+		public static void HorizontalAndVertical(Texture src, RenderTexture dst)
+		{
+			if(reverseUVMaterial == null)
+				InitReverseUVMaterial();
+			SetReverseUVProperty(REVERSE, REVERSE);
+			Blit(src, dst);
+		}
+
+		#endregion PublicMethod
+
+		#region PrivateField
+
+		/// <summary>
+		/// マテリアルを初期化する
+		/// </summary>
+		private static void InitReverseUVMaterial()
+		{
+			var shader = Shader.Find(REVERSE_UV_SHADER);
+			reverseUVMaterial = new Material(shader);
+		}
+
+		/// <summary>
+		/// マテリアルに値を設定する
+		/// </summary>
+		/// <param name="x">x方向の反転値</param>
+		/// <param name="y">y方向の反転値</param>
+		private static void SetReverseUVProperty(float x, float y)
+		{
+			reverseUVMaterial.SetFloat(REVERSE_X, x);
+			reverseUVMaterial.SetFloat(REVERSE_Y, y);
+		}
+
+		private static void Blit(Texture src, RenderTexture dst)
+		{
+			var tmp = RenderTexture.GetTemporary(src.width, src.height, 0);
+			Graphics.Blit(src, tmp, reverseUVMaterial);
+			Graphics.Blit(tmp, dst);
+			RenderTexture.ReleaseTemporary(tmp);
+		}
+
+		#endregion PrivateField
+	}
+}
