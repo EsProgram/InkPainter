55a18bb62e418dd87a07ec942f1cb77d66b4d577
EsProgram <radwimps.september@gmail.com>
2016-11-08 19:27:01 +0900

    UV算出処理の分離

--- Assets/TexturePaint/Script/Core/DynamicCanvas.cs
+++ Assets/TexturePaint/Script/Core/DynamicCanvas.cs
@@ -164,11 +164,8 @@ namespace Es.TexturePaint
 
 		#region MeshData
 
-		private MeshFilter meshFilter;
-		private Mesh mesh;
-		private int[] meshTriangles;
-		private Vector3[] meshVertices;
-		private Vector2[] meshUV;
+		private MeshOperator meshOperator;
+		public MeshOperator MeshOperator { get { return meshOperator; } }
 
 		#endregion MeshData
 
@@ -198,17 +195,7 @@ namespace Es.TexturePaint
 		/// </summary>
 		private void MeshDataCache()
 		{
-			meshFilter = GetComponent<MeshFilter>();
-			mesh = meshFilter.sharedMesh;
-			if(mesh == null)
-			{
-				Debug.LogError("MeshFilterにメッシュが設定されていません");
-				Destroy(this);
-				return;
-			}
-			meshTriangles = mesh.triangles;
-			meshVertices = mesh.vertices;
-			meshUV = mesh.uv;
+			meshOperator = new MeshOperator(GetComponent<MeshFilter>().sharedMesh);
 		}
 
 		/// <summary>
@@ -507,22 +494,7 @@ namespace Es.TexturePaint
 		public bool PaintNearestTriangleSurface(PaintBrush brush, Vector3 worldPos, Camera renderCamera = null)
 		{
 			var p = transform.worldToLocalMatrix.MultiplyPoint(worldPos);
-
-			//頂点の中で一番近いものを含む三角形を取得
-			var tris = Math.GetNearestVerticesTriangle(p, meshVertices, meshTriangles);
-
-			//それぞれの三角形空間でそれっぽいp'を計算
-			var pds = new List<Vector3>();
-			for(int i = 0; i < tris.Length; i += 3)
-			{
-				var i0 = i;
-				var i1 = i + 1;
-				var i2 = i + 2;
-				pds.Add(Math.TriangleSpaceProjection(p, tris[i0], tris[i1], tris[i2]));
-			}
-
-			//pに一番近いp'が求めたかったオブジェクト表面
-			var pd = pds.OrderBy(t => Vector3.Distance(p, t)).First();
+			var pd = meshOperator.NearestLocalSurfacePoint(p);
 
 			return Paint(brush, transform.localToWorldMatrix.MultiplyPoint(pd), renderCamera);
 		}
@@ -539,43 +511,16 @@ namespace Es.TexturePaint
 		/// <returns>ペイント成否</returns>
 		public bool Paint(PaintBrush brush, Vector3 worldPos, Camera renderCamera = null)
 		{
+			Vector2 uv;
+
 			if(renderCamera == null)
 				renderCamera = Camera.main;
 
-			int index0;
-			int index1;
-			int index2;
-			Vector3 t1;
-			Vector3 t2;
-			Vector3 t3;
 			Vector3 p = transform.InverseTransformPoint(worldPos);
-
-			for(var i = 0; i < meshTriangles.Length; i += 3)
-			{
-				index0 = i + 0;
-				index1 = i + 1;
-				index2 = i + 2;
-
-				t1 = meshVertices[meshTriangles[index0]];
-				t2 = meshVertices[meshTriangles[index1]];
-				t3 = meshVertices[meshTriangles[index2]];
-
-				//平面上に存在しない
-				if(!Math.ExistPointInPlane(p, t1, t2, t3))
-					continue;
-				//三角形の辺または内部に存在しない
-				if(!Math.ExistPointOnTriangleEdge(p, t1, t2, t3) && !Math.ExistPointInTriangle(p, t1, t2, t3))
-					continue;
-
-				//UV座標算出
-				var uv1 = meshUV[meshTriangles[index0]];
-				var uv2 = meshUV[meshTriangles[index1]];
-				var uv3 = meshUV[meshTriangles[index2]];
-				Matrix4x4 mvp = renderCamera.projectionMatrix * renderCamera.worldToCameraMatrix * transform.localToWorldMatrix;
-				var uv = Math.TextureCoordinateCalculation(p, t1, uv1, t2, uv2, t3, uv3, mvp);
-
+			Matrix4x4 mvp = renderCamera.projectionMatrix * renderCamera.worldToCameraMatrix * transform.localToWorldMatrix;
+			if(meshOperator.LocalPointToUV(p, mvp, out uv))
 				return PaintUVDirect(brush, uv);
-			}
+
 			return false;
 		}
 
---
+++ Assets/TexturePaint/Script/Core/MeshOperator.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Es.TexturePaint
+{
+	public class MeshOperator
+	{
+		#region MeshData
+
+		private Mesh mesh;
+		private int[] meshTriangles;
+		private Vector3[] meshVertices;
+		private Vector2[] meshUV;
+
+		#endregion MeshData
+
+		#region PublicMethod
+
+		public MeshOperator(Mesh mesh)
+		{
+			MeshDataCache(mesh);
+		}
+
+		/// <summary>
+		/// ローカル座標をUV座標に変換する
+		/// </summary>
+		/// <param name="localPoint">Local-Space Point</param>
+		/// <param name="matrixMVP">World-View-Projection変換行列</param>
+		/// <param name="uv">変換後のUV座標</param>
+		/// <returns>変換の成否</returns>
+		public bool LocalPointToUV(Vector3 localPoint, Matrix4x4 matrixMVP, out Vector2 uv)
+		{
+			int index0;
+			int index1;
+			int index2;
+			Vector3 t1;
+			Vector3 t2;
+			Vector3 t3;
+			Vector3 p = localPoint;
+
+			for(var i = 0; i < meshTriangles.Length; i += 3)
+			{
+				index0 = i + 0;
+				index1 = i + 1;
+				index2 = i + 2;
+
+				t1 = meshVertices[meshTriangles[index0]];
+				t2 = meshVertices[meshTriangles[index1]];
+				t3 = meshVertices[meshTriangles[index2]];
+
+				//平面上に存在しない
+				if(!Math.ExistPointInPlane(p, t1, t2, t3))
+					continue;
+				//三角形の辺または内部に存在しない
+				if(!Math.ExistPointOnTriangleEdge(p, t1, t2, t3) && !Math.ExistPointInTriangle(p, t1, t2, t3))
+					continue;
+
+				//UV座標算出
+				var uv1 = meshUV[meshTriangles[index0]];
+				var uv2 = meshUV[meshTriangles[index1]];
+				var uv3 = meshUV[meshTriangles[index2]];
+				uv = Math.TextureCoordinateCalculation(p, t1, uv1, t2, uv2, t3, uv3, matrixMVP);
+
+				return true;
+			}
+			uv = default(Vector3);
+			return false;
+		}
+
+		/// <summary>
+		/// 指定したLocal-Space上の点に一番近いオブジェクト表面上の点を返す
+		/// </summary>
+		/// <param name="localPoint">Local-Space Point</param>
+		/// <returns>Local-Space Point</returns>
+		public Vector3 NearestLocalSurfacePoint(Vector3 localPoint)
+		{
+			var p = localPoint;
+
+			//頂点の中で一番近いものを含む三角形を取得
+			var tris = Math.GetNearestVerticesTriangle(p, meshVertices, meshTriangles);
+
+			//それぞれの三角形空間でそれっぽいp'を計算
+			var pds = new List<Vector3>();
+			for(int i = 0; i < tris.Length; i += 3)
+			{
+				var i0 = i;
+				var i1 = i + 1;
+				var i2 = i + 2;
+				pds.Add(Math.TriangleSpaceProjection(p, tris[i0], tris[i1], tris[i2]));
+			}
+
+			//pに一番近いp'が求めたかったオブジェクト表面
+			var pd = pds.OrderBy(t => Vector3.Distance(p, t)).First();
+
+			return pd;
+		}
+
+		#endregion PublicMethod
+
+		#region PrivateMethod
+
+		/// <summary>
+		/// メッシュから取得できるデータをキャッシュする
+		/// </summary>
+		private void MeshDataCache(Mesh mesh)
+		{
+			this.mesh = mesh;
+			if(this.mesh == null)
+			{
+				Debug.LogError("MeshFilterにメッシュが設定されていません");
+				return;
+			}
+			meshTriangles = this.mesh.triangles;
+			meshVertices = this.mesh.vertices;
+			meshUV = this.mesh.uv;
+		}
+
+		#endregion PrivateMethod
+	}
+}
