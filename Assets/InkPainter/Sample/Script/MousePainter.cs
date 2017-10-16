b24e17a7d858b0056cf61cbf9b47ef82c2d7ad09
mob-shigehiro <shigehiro@mobcast.jp>
2017-10-16 11:52:34 +0900

    Implementation of paint eraser.

--- Assets/InkPainter/Sample/Script/MousePainter.cs
+++ Assets/InkPainter/Sample/Script/MousePainter.cs
@@ -22,6 +22,9 @@ namespace Es.InkPainter.Sample
 		[SerializeField]
 		private UseMethodType useMethodType = UseMethodType.RaycastHitInfo;
 
+		[SerializeField]
+		bool erase = false;
+
 		private void Update()
 		{
 			if(Input.GetMouseButton(0))
@@ -36,21 +39,21 @@ namespace Es.InkPainter.Sample
 						switch(useMethodType)
 						{
 							case UseMethodType.RaycastHitInfo:
-								success = paintObject.Paint(brush, hitInfo);
+								success = erase ? paintObject.Erase(brush, hitInfo) : paintObject.Paint(brush, hitInfo);
 								break;
 
 							case UseMethodType.WorldPoint:
-								success = paintObject.Paint(brush, hitInfo.point);
+								success = erase ? paintObject.Erase(brush, hitInfo.point) : paintObject.Paint(brush, hitInfo.point);
 								break;
 
 							case UseMethodType.NearestSurfacePoint:
-								success = paintObject.PaintNearestTriangleSurface(brush, hitInfo.point);
+								success = erase ? paintObject.EraseNearestTriangleSurface(brush, hitInfo.point) : paintObject.PaintNearestTriangleSurface(brush, hitInfo.point);
 								break;
 
 							case UseMethodType.DirectUV:
 								if(!(hitInfo.collider is MeshCollider))
 									Debug.LogWarning("Raycast may be unexpected if you do not use MeshCollider.");
-								success = paintObject.PaintUVDirect(brush, hitInfo.textureCoord);
+								success = erase ? paintObject.EraseUVDirect(brush, hitInfo.textureCoord) : paintObject.PaintUVDirect(brush, hitInfo.textureCoord);
 								break;
 						}
 					if(!success)
